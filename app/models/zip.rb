d6974501ccbc3cb0cac3e957b167bfbe283181f1
johnalvn1 <jean1052@gmail.com>
2016-03-28 20:59:27 +0000

    Add method all in zip class

--- app/models/zip.rb
+++ app/models/zip.rb
@@ -1,4 +1,26 @@
 class Zips
+    def self.all(prototype = {}, sort={:population => 1}, offset = 0, limit=100)
+        tmp = {}
+        sort.each do |k,v|
+            k = k.to_sym == :population ? :pop : k.to_sym
+            tmp[k] = v if [:city, :state, :pop].include?(k)
+        end
+        sort = tmp
+        prototype = prototype.symbolize_keys.slice(:city, :state) if !prototype.nil?
+        Rails.logger.debug {"getting all zips, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
+        
+        result = collection.find(prototype)
+            .projection({ _id: true, city: true, state: true, pop: true})
+            .sort(sort)
+            .skip(offset)
+            
+        result = result.limit(limit) if !limit.nil?
+        
+        return result
+    end
+    
+    
+    
     # convinience method for access to client in console
     def self.mongo_client
         Mongoid::Clients.default
